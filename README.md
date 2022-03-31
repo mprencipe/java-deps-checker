f716f42181222072edd0a70b3a71ef5cf8bb8ee6
Massimo Prencipe <mprencipe@gmail.com>
2022-03-31 22:17:52 +0300

    Add version support

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A simple script to go search an organization's Java dependency management files for a particular dependency. Maven pom.xml and Gradle build.gradle are currently supported, though support for the latter can be improved.
 
 ## Installation
+Requires Python 3.
 ```
 virtualenv venv
 source ./venv/bin/activate
@@ -10,5 +11,6 @@ pip install -r requirements.txt
 
 ## Usage
 ```
-python3 java-deps-checker.py <your_github_access_token> <github_org_name> <dependency_group_id> <dependency_artifact_id>
+./java-deps-checker.py <your_github_access_token> <github_org_name> <dependency_group_id> <dependency_artifact_id> <dependency_version_id>
 ```
+The version id is optional.
--- java-deps-checker.py
+++ java-deps-checker.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python3
+
 import sys, base64
 from github import Github, ContentFile, Repository
 from xml.dom import minidom
 from typing import Dict
 
+if len(sys.argv) < 5:
+    print('Usage:')
+    print('./java-deps-checker.py <your_github_access_token> <github_org_name> <dependency_group_id> <dependency_artifact_id> <dependency_version_id>')
+    print('The version id is optional')
+    sys.exit(0)
+
 access_token = sys.argv[1]
 organization = sys.argv[2]
 group_id = sys.argv[3]
 artifact_id = sys.argv[4]
+version_id = sys.argv[5] if len(sys.argv) > 5 else None
 
 POM = 'pom.xml'
 GRADLE = 'build.gradle'
@@ -19,17 +28,27 @@ def report_found(file: ContentFile, repo: Repository):
     print(f'Found in repository {repo.name}, file {file.name}')
 
 def find_in_pom(file: ContentFile, repo: Repository, dep: Dict):
+    version_specified = dep['version_id'] is not None
     doc = minidom.parseString(base64.b64decode(file.content).decode('utf-8'))
     pom_deps = doc.getElementsByTagName('dependency')
     for pom_dep in pom_deps:
         artifact_id = pom_dep.getElementsByTagName('artifactId')[0].firstChild.data
         group_id = pom_dep.getElementsByTagName('groupId')[0].firstChild.data
-        if dep['artifact_id'] == artifact_id and dep['group_id'] == group_id:
-            report_found(file, repo)
+        if dep['artifact_id'] == artifact_id and dep['group_id'] == group_id:    
+            if version_specified:
+                version_id_elem = pom_dep.getElementsByTagName('version')
+                if len(version_id_elem) > 0:
+                    version_id = version_id_elem[0].firstChild.data
+                    if version_id == dep['version_id']:
+                        report_found(file, repo)
+            else:
+                report_found(file, repo)
 
 def find_in_gradle(file: ContentFile, repo: Repository, dep: Dict):
+    version_specified = dep['version_id'] is not None
+    version_line = f"{dep['group_id']}:{dep['artifact_id']}:{dep['version_id']}" if version_specified else f"{dep['group_id']}:{dep['artifact_id']}"
     for line in base64.b64decode(file.content).decode('utf-8').splitlines():
-        if f"{dep['group_id']}:{dep['artifact_id']}" in line:
+        if version_line in line:
             report_found(file, repo)
 
 def process_dependencies(file: ContentFile, repo: Repository, dep: Dict, g: Github):
@@ -50,4 +69,4 @@ def process_org_repos(org: str, dep: Dict, g: Github):
                 process_dependencies(file, repo, dep, g)
 
 print(f'Starting search in GitHub organization {organization} for artifact {group_id}:{artifact_id}')
-process_org_repos(organization, {'group_id': group_id, 'artifact_id': artifact_id}, g)
+process_org_repos(organization, {'group_id': group_id, 'artifact_id': artifact_id, 'version_id': version_id}, g)
